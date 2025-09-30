7604b424839f1102bd20d8d98fae2c419bf8ff77
PandesalPanpan <petermarticio@gmail.com>
2025-09-30 09:41:24 +0800

    Feat: Add a check if ship is instance of Ship Class and checking if coordinates is valid

--- src/gameboard.js
+++ src/gameboard.js
@@ -9,11 +9,23 @@ export default class GameBoard {
     getShips = () => {
         return this.ships;
     }
-    
-    placeShip = (ship, x, y) => {
+
+    placeShip = (ship, x, y, isHorizontal = true) => {
+        // check if invalid coordinates
+
+        const isInstanceOfShip = ship instanceof Ship;
+        const isInvalidCoordinates = x < 0 || x > 9 || y < 0 || y > 9;
+        if (isInvalidCoordinates || !isInstanceOfShip) {
+            return;
+        }
+
         // get the ship length
         for (let i = 0; i < ship.length; i++) {
-            this.board[x][y+i] = 1;
+            if (!isHorizontal) {
+                this.board[x + i][y] = 1;
+            } else {
+                this.board[x][y + i] = 1;
+            }
         }
         this.ships.push(ship);
 
