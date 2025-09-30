0718d81346f93804a8eef6b0e4fa50a81d7dde16
PandesalPanpan <petermarticio@gmail.com>
2025-09-30 08:02:19 +0800

    Feat: Add Ship Class with length constructor

---
+++ src/ship.js
@@ -0,0 +1,12 @@
+export default class Ship {
+    // length
+    // amount of times hit
+    // isSunk
+
+    constructor(length) {
+        this.length = length > 0 ? length : 1;
+    }
+
+
+    // hit method - increases amount of hit
+}
