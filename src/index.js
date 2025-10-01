64026dab3218d557e7b20c692b7b8da248b19a56
PandesalPanpan <petermarticio@gmail.com>
2025-10-02 05:33:38 +0800

    Feat: Create player1 container on start

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "./styles.css";
+import UserInterface from "./user-interface";
 
+
+const screen = new UserInterface();
+screen.initialize();
--- src/user-interface.js
+++ src/user-interface.js
@@ -3,9 +3,14 @@
 // Start game button and initialize two players
 // Display two gameboards
 
+import Player from "./player";
+import Ship from "./ship";
+
 export default class UserInterface {
     constructor() {
         this.startButton = null;
+        this.player1 = null;
+        this.player2 = null;
     }
 
     initialize = () => {
@@ -16,7 +21,7 @@ export default class UserInterface {
     createStartButton = () => {
         this.startButton = document.createElement('button');
         this.startButton.textContent = 'Start Game'
-        document.body.appendChild(this.startButton);            
+        document.body.appendChild(this.startButton);
     }
 
     attachEventListener = () => {
@@ -24,6 +29,61 @@ export default class UserInterface {
     }
 
     handleStartButton = () => {
-        console.log('Handling Start Button');
+        if (this.gameContainer) return;
+
+        // Create the two players
+        this.player1 = new Player();
+        this.player2 = new Player();
+
+        this.createGameContainer();
+        this.createUsersContainer();
+
+        // Display board based on users
+        this.displayUserGameboard();
     }
+
+    createGameContainer = () => {
+        this.gameContainer = document.createElement('div');
+        this.gameContainer.classList.add('game-container');
+        document.body.appendChild(this.gameContainer);
+    }
+
+    displayUserGameboard = () => {
+        // Grab the user board
+        const board = this.player1.gameBoard.board;
+        this.player1.gameBoard.placeShip(new Ship(), 0, 0);
+
+
+
+
+        // For each row array elements, place a cell
+        board.forEach((row) => {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            row.forEach((cell) => {
+                const box = document.createElement('div');
+                box.classList.add('cell');
+                box.textContent = cell ? 'o' : ' ';
+                column.appendChild(box);
+            })
+            this.player1Container.appendChild(column);
+        })
+        // Display it
+    }
+
+    createUsersContainer = () => {
+        // Create user container
+        this.player1Container = document.createElement('div');
+        this.player1Container.id = 'player-1-board';
+        this.player1Container.classList.add('container');
+
+        this.player2Container = document.createElement('div');
+        this.player2Container.id = 'player-2-board';
+        this.player2Container.classList.add('container');
+
+        this.gameContainer.appendChild(this.player1Container);
+        this.gameContainer.appendChild(this.player2Container);
+    }
+
+
 }
